07fb9ee5c0ee97728a5c504e9e743d75d8c24970
Stanley Chan <25721992+stanley0010@users.noreply.github.com>
2024-03-14 16:15:36 +0800

    Create 3 Pages Fixes #44

--- core/lib/gui/app.py
+++ core/lib/gui/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+@app.route("/create")
+def create_scenario():
+    return render_template('create_scenario.html', title="Create Scenario")
+
+@app.route("/import")
+def import_scenario():
+    return render_template('import_scenario.html', title="Import Scenario")
